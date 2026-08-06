14ff63f1d4dfebbc6c0dce0e770f9a94e2be6aaf
ashbbdu <ashishsrivastava7007@gmail.com>
2026-08-07 00:35:09 +0530

    JwtAuthenticationEntryPoint

---
+++ src/main/java/com/project_management/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,49 @@
+package com.project_management.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project_management.advices.ApiError;
+import com.project_management.advices.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+//        String body = """
+//            {
+//                "status":401,
+//                "error":"Unauthorized",
+//                "message":"Authentication is required to access this resource."
+//            }
+//            """;
+//        response.getWriter().write(body);
+//        ApiError apiError = ApiError.builder()
+//                .httpStatus(HttpStatus.UNAUTHORIZED)
+//                .message("Authentication is required to access this resource.")
+//                .build();
+//
+//        ApiResponse<?> apiResponse = ApiResponse.error("Authentication Failed" , apiError);
+//
+//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        response.setContentType("application/json");
+//
+//        objectMapper.writeValue(
+//                response.getOutputStream(),
+//                apiResponse
+//        );
+        return;
+    }
+}
